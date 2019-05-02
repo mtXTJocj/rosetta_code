36565e27fcd93ef37a59c8eaa26a7b8b7f3a6f06
mtXTJocj <mtXTJocj@yahoo.co.jp>
2019-05-02 20:20:00 +0900

    implemented "100 doors" task.

---
+++ _100_doors/README.md
@@ -0,0 +1,1 @@
+[100 Doors](http://www.rosettacode.org/wiki/100_doors)
--- _100_doors/src/main.rs
+++ _100_doors/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let mut doors = [false; 100];
+
+    for stride in 1..101 {
+        let mut idx = stride;
+        while idx <= 100 {
+            doors[idx - 1] = !doors[idx - 1];
+            idx += stride;
+        }
+    }
+
+    for (i, &is_opend) in doors.iter().enumerate() {
+        println!(
+            "Door {} is {}",
+            i + 1,
+            if is_opend { "opened" } else { "closed" }
+        );
+    }
 }
